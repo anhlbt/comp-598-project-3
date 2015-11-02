e0c0676c09e5ccae130bbb7a4c765fc58232bb0c
Charles Bloomfield <charles.bloomfield@mail.mcgill.ca>
2015-11-02 18:06:42 -0500

    added memoizing to data manager. improved driver to support dev_mode testing on 1 percent of a specified dataset.

--- src/config/default.py
+++ src/config/default.py
@@ -1,31 +1,29 @@
 __author__ = 'Charlie'
 
-#classifiers
+# classifiers
 from sklearn.svm import LinearSVC
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 
-#feature selection
-from sklearn.feature_selection import SelectPercentile, chi2, f_classif 
+# feature selection
+from sklearn.feature_selection import SelectPercentile, chi2, f_classif
 # from sklearn.pipeline import FeatureUnion
 # from sklearn.decomposition import PCA
 
 
 selectors = {
-	('percentile', SelectPercentile()): {
-		'percentile__percentile': (1, 5, 25, 50, 100),
-		'percentile__score_func': (chi2, f_classif)
-	}
+    ('percentile', SelectPercentile()): {
+        'percentile__percentile': (1, 5, 25, 50, 100),
+        'percentile__score_func': (chi2, f_classif)
+    }
 }
 
-
 learners = {
-	('svc', LinearSVC()): {
-		'svc__C': (1.0, 10.0)
-	},
+    ('svc', LinearSVC()): {
+        'svc__C': (1.0, 10.0)
+    },
 
-	('lr', LogisticRegression()): {
-		'lr__penalty': ('l1', 'l2'),
-		'lr__C': (1.0, 2.0)
-	}
+    ('lr', LogisticRegression()): {
+        'lr__penalty': ('l1', 'l2'),
+        'lr__C': (1.0, 2.0)
+    }
 }
--- src/data_manager.py
+++ src/data_manager.py
@@ -1,4 +1,4 @@
-__author__ = "Charlie"
+__author__ = "Charlie, Josh"
 
 from sklearn import datasets
 import numpy as np
@@ -6,26 +6,54 @@ import numpy as np
 train_inputs1 = './data/train_inputs1.npz'
 train_inputs2 = './data/train_inputs2.npz'
 train_outputs = './data/train_outputs.npz'
-test_inputs   = './data/test_inputs.npz'
+test_inputs = './data/test_inputs.npz'
 
 
-def load_iris_data():
-	iris = datasets.load_iris()
-	return np.matrix(iris.data), np.array(iris.target)
+def memoize(func):
+    """
+    @param func: function to be decorated
+    @ return: decorated function
+    """
+    cache = {}
+    def wrapped(*args):
+        if args not in cache:
+            cache[args] = func(*args)
+        return cache[args]
+    return wrapped
 
 
 def load_test_data():
-        X1 = np.load(train_inputs1)['train_inputs1_np']
-        X2 = np.load(train_inputs2)['train_inputs2_np']
-        X  = np.vstack((X1,X2))
-        Y  = np.load(train_outputs)['train_outputs_np']
-        return X,Y
+    """
+    :return:
+    """
+    X1 = np.load(train_inputs1)['train_inputs1_np']
+    X2 = np.load(train_inputs2)['train_inputs2_np']
+    X = np.vstack((X1, X2))
+    Y = np.load(train_outputs)['train_outputs_np']
+    return X, Y
+
+
+@memoize
+def load_iris_data():
+    """
+    For testing purposes. Loads a small dataset from sklearn.
+    """
+    iris = datasets.load_iris()
+    return np.matrix(iris.data), np.array(iris.target)
+
+
+@memoize
+def load_raw_data():
+    """
+    Delegates to load_test_data
+    """
+    return load_test_data()
 
 
 def available_datasets():
-	"""
-	"""
-	return [
-		('iris', load_iris_data),
-		('kaggle', load_test_data)
-	]
+    """
+    """
+    return [
+        ('iris', load_iris_data),
+        ('kaggle images', load_raw_data)
+    ]
