f02ae62b07b44fde8c2b54b9bcfd76977b01ef35
Charles Bloomfield <charles.bloomfield@mail.mcgill.ca>
2015-11-09 17:56:01 -0500

    added image rotation generation

--- src/data_manager.py
+++ src/data_manager.py
@@ -2,6 +2,8 @@ __author__ = "Charlie, Josh"
 
 from sklearn import datasets
 import numpy as np
+from scipy.ndimage.interpolation import rotate
+from PIL import Image
 
 train_inputs1 = './data/train_inputs1.npz'
 train_inputs2 = './data/train_inputs2.npz'
@@ -48,6 +50,43 @@ def load_raw_data():
     Delegates to load_test_data
     """
     return load_test_data()
+
+
+def _rotations(image, num_rotations):
+    """
+    :param image:
+    :return: array of rotated images
+    """
+    angle = 360 / num_rotations
+    rotations = np.empty((num_rotations, image.shape[0], image.shape[1]))
+    rotated_image = image
+    for i in range(num_rotations):
+        rotations[i] = rotated_image
+        rotated_image = rotate(rotated_image, angle)
+    return rotations
+
+
+def generate_rotated_images(images, classes):
+    """
+    :param images: 3d array of numpy array images (n x 48 x 48)
+    :param classes: 1d array of image classifications
+    :return: tuple np.array of rotated images
+    """
+    num_rotations = 4
+    rotated_images = []
+    for index in range(images.shape[0]):
+        image = images[index,:,:]
+        rotated_images.append(_rotations(image, num_rotations))
+    return np.vstack(tuple(rotated_images)), np.repeat(classes, num_rotations)
+
+
+def load_rotated_images():
+    """
+    :return: tuple of 3d numpy array of images and 1d numpy array of images
+    """
+    images, classes = load_raw_data()
+    images = np.reshape(images (-1, 48, 48))
+    return generate_rotated_images(images, classes)
 
 
 def available_datasets():
