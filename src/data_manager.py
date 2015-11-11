0c316d527bfffb1f92f0d94ab2ed4ff31e310354
Charles Bloomfield <charles.bloomfield@mail.mcgill.ca>
2015-11-11 00:36:16 -0500

    added option to not rotate images to save memory

--- src/data_manager.py
+++ src/data_manager.py
@@ -57,7 +57,7 @@ def load_iris_data():
     return np.matrix(iris.data), np.array(iris.target)
 
 
-def load_all_images():
+def load_all_images(rotate=True):
     """
     Concatenated and shuffles images returned by load_raw_data, load_raw_mnist_images, and load_transformed_images
     :return: all available images as np.array (n x 48 x 48) where n is the number of available images
@@ -67,8 +67,9 @@ def load_all_images():
     raw_imgs, raw_clss = load_raw_data()
     imgs, clss = np.vstack(raw_imgs, mnist_imgs), np.vstack(raw_clss,  mnist_clss)
     imgs, clss = shuffle(imgs, clss)
-    imgs, clss = generate_rotated_images(imgs, clss)
     imgs, clss = generate_trimmed_images(imgs, clss)
+    if filter:
+        imgs, clss = generate_rotated_images(imgs, clss)
     return imgs, clss
 
 
